7d7e63e2e8ca05b93e514743d6fbf7c16edc6b16
pividal <pividal@pichincha.com>
2025-09-19 13:15:40 -0500

    refactor(Microservice): Entities
    - Use ReactiveCrudRepository
    
    BREAKING CHANGE: [DESSIGNPATTERN-001](https://develcorp.com.ec/browse/DESSIGNPATTERN-0001)

--- src/main/java/org/develcorp/learn/library/infrastructure/adapter/output/repository/BookRepository.java
+++ src/main/java/org/develcorp/learn/library/infrastructure/adapter/output/repository/BookRepository.java
@@ -1,59 +1,14 @@
 package org.develcorp.learn.library.infrastructure.adapter.output.repository;
 
-import io.r2dbc.spi.ConnectionFactory;
 import org.develcorp.learn.library.domain.model.entity.Book;
-import org.develcorp.learn.library.infrastructure.adapter.output.config.DatabaseConnection;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 /**
  * Repository interface for managing Book entities.
  */
-public class BookRepository {
-
-    private final ConnectionFactory connectionFactory;
-
-    public BookRepository() {
-        this.connectionFactory = DatabaseConnection.getInstance().getConnectionPool();
-    }
-
-    /**
-     * Save a book to the repository.
-     *
-     * @param book the book to save
-     * @return a Mono indicating the completion of the save operation
-     */
-    public Mono<Void> save(Book book) {
-        return Mono.from(connectionFactory.create())
-                .flatMap(connection -> connection.createStatement("INSERT INTO books (id, title, author, type, format, status) VALUES ($1, $2, $3, $4, $5, $6)")
-                        .bind("$1", book.getId())
-                        .bind("$2", book.getTitle())
-                        .bind("$3", book.getAuthor())
-                        .bind("$4", book.getType().toString())
-                        .bind("$5", book.getFormat().toString())
-                        .bind("$6", book.getStatus().toString())
-                        .execute())
-                .then();
-    }
-
-    /**
-     * Find all books in the repository.
-     *
-     * @return a Flux of all books
-     */
-    public Flux<Book> findAll() {
-        return Flux.from(connectionFactory.create())
-                .flatMap(connection -> connection.createStatement("SELECT * FROM books")
-                        .execute())
-                .flatMap(result -> result.map((row, rowMetadata) -> Book.builder()
-                        .id(row.get("id", Long.class))
-                        .title(row.get("title", String.class))
-                        .author(row.get("author", String.class))
-                        .type(BookType.valueOf(row.get("type", String.class)))
-                        .format(BookFormat.valueOf(row.get("format", String.class)))
-                        .status(BookStatus.valueOf(row.get("status", String.class)))
-                        .build()));
-    }
+public interface BookRepository extends ReactiveCrudRepository<Book, Long> {
 
     /**
      * Find books by their title.
@@ -61,18 +16,5 @@ public class BookRepository {
      * @param title the title of the book
      * @return a Flux of books matching the title
      */
-    public Flux<Book> findByTitle(String title) {
-        return Flux.from(connectionFactory.create())
-                .flatMap(connection -> connection.createStatement("SELECT * FROM books WHERE title = $1")
-                        .bind("$1", title)
-                        .execute())
-                .flatMap(result -> result.map((row, rowMetadata) -> Book.builder()
-                        .id(row.get("id", Long.class))
-                        .title(row.get("title", String.class))
-                        .author(row.get("author", String.class))
-                        .type(BookType.valueOf(row.get("type", String.class)))
-                        .format(BookFormat.valueOf(row.get("format", String.class)))
-                        .status(BookStatus.valueOf(row.get("status", String.class)))
-                        .build()));
-    }
+    Flux<Book> findByTitle(String title);
 }
--- src/main/java/org/develcorp/learn/library/infrastructure/adapter/output/repository/LoanRepository.java
+++ src/main/java/org/develcorp/learn/library/infrastructure/adapter/output/repository/LoanRepository.java
@@ -1,57 +1,14 @@
 package org.develcorp.learn.library.infrastructure.adapter.output.repository;
 
-import io.r2dbc.spi.ConnectionFactory;
 import org.develcorp.learn.library.domain.model.entity.Loan;
-import org.develcorp.learn.library.infrastructure.adapter.output.config.DatabaseConnection;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 /**
  * Repository interface for managing Loan entities.
  */
-public class LoanRepository {
-
-    private final ConnectionFactory connectionFactory;
-
-    public LoanRepository() {
-        this.connectionFactory = DatabaseConnection.getInstance().getConnectionPool();
-    }
-
-    /**
-     * Save a loan to the database.
-     *
-     * @param loan the loan to save
-     * @return a Mono indicating the completion of the save operation
-     */
-    public Mono<Void> save(Loan loan) {
-        return Mono.from(connectionFactory.create())
-                .flatMap(connection -> connection.createStatement("INSERT INTO loans (id, book_id, loan_date, return_date, borrower) VALUES ($1, $2, $3, $4, $5)")
-                        .bind("$1", loan.getId())
-                        .bind("$2", loan.getBookId())
-                        .bind("$3", loan.getLoanDate())
-                        .bind("$4", loan.getReturnDate())
-                        .bind("$5", loan.getBorrower())
-                        .execute())
-                .then();
-    }
-
-    /**
-     * Find all loans in the database.
-     *
-     * @return a Flux of all loans
-     */
-    public Flux<Loan> findAll() {
-        return Flux.from(connectionFactory.create())
-                .flatMap(connection -> connection.createStatement("SELECT * FROM loans")
-                        .execute())
-                .flatMap(result -> result.map((row, rowMetadata) -> Loan.builder()
-                        .id(row.get("id", Long.class))
-                        .bookId(row.get("book_id", Long.class))
-                        .loanDate(row.get("loan_date", LocalDate.class))
-                        .returnDate(row.get("return_date", LocalDate.class))
-                        .borrower(row.get("borrower", String.class))
-                        .build()));
-    }
+public interface LoanRepository extends ReactiveCrudRepository<Loan, Long> {
 
     /**
      * Find loans by the borrower's name.
@@ -59,19 +16,7 @@ public class LoanRepository {
      * @param borrower the name of the borrower
      * @return a Flux of loans matching the borrower
      */
-    public Flux<Loan> findByBorrower(String borrower) {
-        return Flux.from(connectionFactory.create())
-                .flatMap(connection -> connection.createStatement("SELECT * FROM loans WHERE borrower = $1")
-                        .bind("$1", borrower)
-                        .execute())
-                .flatMap(result -> result.map((row, rowMetadata) -> Loan.builder()
-                        .id(row.get("id", Long.class))
-                        .bookId(row.get("book_id", Long.class))
-                        .loanDate(row.get("loan_date", LocalDate.class))
-                        .returnDate(row.get("return_date", LocalDate.class))
-                        .borrower(row.get("borrower", String.class))
-                        .build()));
-    }
+    Flux<Loan> findByBorrower(String borrower);
 
     /**
      * Find loans by book ID.
@@ -79,17 +24,5 @@ public class LoanRepository {
      * @param bookId the ID of the book
      * @return a Flux of loans matching the book ID
      */
-    public Flux<Loan> findByBookId(Long bookId) {
-        return Flux.from(connectionFactory.create())
-                .flatMap(connection -> connection.createStatement("SELECT * FROM loans WHERE book_id = $1")
-                        .bind("$1", bookId)
-                        .execute())
-                .flatMap(result -> result.map((row, rowMetadata) -> Loan.builder()
-                        .id(row.get("id", Long.class))
-                        .bookId(row.get("book_id", Long.class))
-                        .loanDate(row.get("loan_date", LocalDate.class))
-                        .returnDate(row.get("return_date", LocalDate.class))
-                        .borrower(row.get("borrower", String.class))
-                        .build()));
-    }
+    Flux<Loan> findByBookId(Long bookId);
 }
